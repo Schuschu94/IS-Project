84a4cb73d9ecf0af45a7af7be069356cf25f6daf
Schuschu94 <50869068+Schuschu94@users.noreply.github.com>
2019-05-28 16:20:04 +0200

    Add files via upload
    Im Logic.js File mit den Transaktionen begonnen

--- oshealthrec-network/lib/logic.js
+++ oshealthrec-network/lib/logic.js
@@ -1,43 +1,50 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
 /**
- * Write your transction processor functions here
+ * 
+ * Die Logik hinter den Transaktionen implementieren
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 
-/**
- * Sample transaction
- * @param {org.oshealthrec.network.SampleTransaction} sampleTransaction
- * @transaction
- */
-async function sampleTransaction(tx) {
-    // Save the old value of the asset.
-    const oldValue = tx.asset.value;
 
-    // Update the asset with the new value.
-    tx.asset.value = tx.newValue;
+ /**
+  * 
+  *  @param {org.oshealthrec.network.patient_add_doctor} patient_add_doctor 
+  * @transaction
+  * 
+  */
 
-    // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.oshealthrec.network.SampleAsset');
-    // Update the asset in the asset registry.
-    await assetRegistry.update(tx.asset);
+ async function patient_add_doctor(patient_add_doctor) {
 
-    // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.oshealthrec.network', 'SampleEvent');
-    event.asset = tx.asset;
-    event.oldValue = oldValue;
-    event.newValue = tx.newValue;
-    emit(event);
+    //if(patient_add_doctor.patient == getCurrentPatient){                      // Nur angemeldete Participant kann den Befehl ausführen
+    patient_add_doctor.patient.doctors.push(patient_add_doctor.doctor);         // Doctor array patient hinzugefügen
+        let participantRegistry = await                                         // Der await Ausdruck lässt async Funktionen pausieren, bis
+       getParticipantRegistry('org.oshealthrec.network.Patient');               // ein Promise erfüllt oder abgewiesen ist, und führt die async
+        await participantRegistry.update(patient_add_doctor.patient);           // danach weiter aus.
+      }
+    // }
+
+ /**
+  * 
+  * @param {org.oshealthrec.network.patient_delete_doctor} patient_delete_doctor 
+  * @transaction
+  * 
+  */
+
+async function patient_delete_doctor(patient_delete_doctor) {
+
+    let doctorarray = patient_delete_doctor.patient.doctors;
+    let index = doctorarray.indexOf(patient_delete_doctor.doctor);       
+    if(index >-1){
+        doctorarray.splice(index, 1);
+      
+    }
+
+    let participantRegistry = await
+    getParticipantRegistry('org.oshealthrec.network.Patient');
+    await participantRegistry.update(patient_delete_doctor.patient);    
 }
+    
