fcd0f9da04e5f054349b4fc904a397c7e5d4c2ba
Estobe <janstb@web.de>
2019-08-12 14:55:47 +0200

    github-auth-server überarbeitet

--- github-auth-server.js
+++ github-auth-server.js
@@ -3,10 +3,10 @@ var app = express();
 const fetch = require("node-fetch");
 
 
-app.post('/token/:code', function (req, res) {
-    let code = req.params.code.toString();
-    let token = "";
-    fetch('https://github.com/login/oauth/access_token', {
+app.post('/token/:code', async function (req, res) {
+    let code = req.params.code;
+
+    const response = await fetch('https://github.com/login/oauth/access_token', {
         method: 'post',
         body: {
             'client_id': '2e80edb9aac1beb5e0cf',
@@ -16,16 +16,28 @@ app.post('/token/:code', function (req, res) {
         headers: {
             'Content-Type': 'application/json'
         }
-    }).then(function (response) {
-        console.log(code);
-        token = response;
-        return token;
-    }).then(function (tokenData) {
-        console.log(tokenData);
-        res.end(token);
-    }).catch(function () {
-        console.log("Token nicht erhalten");
     });
+
+    res.end(response);
+
+    // fetch('https://github.com/login/oauth/access_token', {
+    //     method: 'post',
+    //     body: {
+    //         'client_id': '2e80edb9aac1beb5e0cf',
+    //         'client_secret': '19409ed971477475776611a5079a34d2ca0cc360',
+    //         'code': code
+    //     },
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(function (response) {
+    //     return response;
+    // }).then(function (tokenData) {
+    //     console.log(tokenData);
+    //     res.end(token);
+    // }).catch(function () {
+    //     console.log("Token nicht erhalten");
+    // });
 });
 
 var server = app.listen(8081, function () {
