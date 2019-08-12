7626e8b88b9fecd04e10dbc5db802a72a8bd91e2
Estobe <janstb@web.de>
2019-08-12 15:49:10 +0200

    github-auth-server überarbeitet

--- github-auth-server.js
+++ github-auth-server.js
@@ -1,50 +1,91 @@
-var express = require('express');
-var app = express();
-const fetch = require("node-fetch");
+const express = require('express')
+const app = express()
+
+// Import the axios library, to make HTTP requests
+const axios = require('axios')
+
+// This is the client ID and client secret that you obtained
+// while registering the application
+const clientID = '2e80edb9aac1beb5e0cf'
+const clientSecret = '19409ed971477475776611a5079a34d2ca0cc360'
 
 
-app.post('/token/:code', async function (req, res) {
-    let code = req.params.code;
+// Declare the redirect route
+app.get('/home', (req, res) => {
 
-    const response = await fetch('https://github.com/login/oauth/access_token', {
+    // The req.query object has the query params that were sent to this route.
+    const requestToken = req.query.code
+
+    axios({
         method: 'post',
-        body: {
-            'client_id': '2e80edb9aac1beb5e0cf',
-            'client_secret': '19409ed971477475776611a5079a34d2ca0cc360',
-            'code': code
-        },
+        url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+        // Set the content type header, so that we get the response in JSON
         headers: {
-            'Content-Type': 'application/json'
-        },
-    });
+            accept: 'application/json'
+        }
 
-    const token = await response.toString();
-    console.log(token);
+    }).then((response) => {
 
-    res.end(token);
+        const accessToken = response.data.access_token
+        console.log(response.data)
 
-    // fetch('https://github.com/login/oauth/access_token', {
-    //     method: 'post',
-    //     body: {
-    //         'client_id': '2e80edb9aac1beb5e0cf',
-    //         'client_secret': '19409ed971477475776611a5079a34d2ca0cc360',
-    //         'code': code
-    //     },
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // }).then(function (response) {
-    //     return response;
-    // }).then(function (tokenData) {
-    //     console.log(tokenData);
-    //     res.end(token);
-    // }).catch(function () {
-    //     console.log("Token nicht erhalten");
-    // });
-});
+        // redirect the user to the home page, along with the access token
+        res.redirect(`/index.html?access_token=${accessToken}`)
+    })
+})
 
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port)
-});
+app.use(express.static(__dirname))
+app.listen(8081,()=>{
+    console.log("Server listening on port : 8081")
+})
+
+// var express = require('express');
+// var app = express();
+// const fetch = require("node-fetch");
+//
+//
+// app.post('/token/:code', async function (req, res) {
+//     let code = req.params.code;
+//
+//     const response = await fetch('https://github.com/login/oauth/access_token', {
+//         method: 'post',
+//         body: {
+//             'client_id': '2e80edb9aac1beb5e0cf',
+//             'client_secret': '19409ed971477475776611a5079a34d2ca0cc360',
+//             'code': code
+//         },
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//     });
+//
+//     const token = await response.toString();
+//     console.log(token);
+//
+//     res.end(token);
+//
+//     // fetch('https://github.com/login/oauth/access_token', {
+//     //     method: 'post',
+//     //     body: {
+//     //         'client_id': '2e80edb9aac1beb5e0cf',
+//     //         'client_secret': '19409ed971477475776611a5079a34d2ca0cc360',
+//     //         'code': code
+//     //     },
+//     //     headers: {
+//     //         'Content-Type': 'application/json'
+//     //     }
+//     // }).then(function (response) {
+//     //     return response;
+//     // }).then(function (tokenData) {
+//     //     console.log(tokenData);
+//     //     res.end(token);
+//     // }).catch(function () {
+//     //     console.log("Token nicht erhalten");
+//     // });
+// });
+//
+// var server = app.listen(8081, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("Example app listening at http://%s:%s", host, port)
+// });
