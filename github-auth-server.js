efeadc40adc8b448814763b0bee1ddb28f3e6d97
Estobe <janstb@web.de>
2019-08-12 15:12:18 +0200

    github-auth-server überarbeitet

--- github-auth-server.js
+++ github-auth-server.js
@@ -6,7 +6,7 @@ const fetch = require("node-fetch");
 app.post('/token/:code', async function (req, res) {
     let code = req.params.code;
 
-    const response = await fetch('https://github.com/login/oauth/access_token', {
+    const response = await fetch('https://crossorigin.me/https://github.com/login/oauth/access_token', {
         method: 'post',
         body: {
             'client_id': '2e80edb9aac1beb5e0cf',
@@ -14,10 +14,8 @@ app.post('/token/:code', async function (req, res) {
             'code': code
         },
         headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+            'Content-Type': 'application/json'
         },
-        mode: 'no-cors'
     });
 
     const token = await response.toString();
