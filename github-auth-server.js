0ab1222b2330af551932a07ccf558617a8667c6f
Estobe <janstb@web.de>
2019-08-13 15:50:29 +0200

    github-auth-server überarbeitet

--- github-auth-server.js
+++ github-auth-server.js
@@ -6,7 +6,7 @@ const request = require('request');
 
 app.use(cors())
 
-app.get('/token/:code', function (req, res) {
+app.get('/token/:code', async function (req, res) {
     let code = req.params.code;
 
     options = {
