6820fea034ab0a3d17b4023d2db1bb14f1cb8778
Estobe <janstb@web.de>
2019-08-13 15:12:57 +0200

    github-auth-server überarbeitet

--- github-auth-server.js
+++ github-auth-server.js
@@ -9,25 +9,7 @@ app.use(cors())
 app.post('/token/:code', async function (req, res) {
     let code = req.params.code;
 
-    // const response = await fetch('https://github.com/login/oauth/access_token', {
-    //     method: 'post',
-    //     body: {
-    //         client_id: '2e80edb9aac1beb5e0cf',
-    //         client_secret: '19409ed971477475776611a5079a34d2ca0cc360',
-    //         code: code
-    //     },
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    // });
-    //
-    // const token = await JSON.stringify(response.body);
-    // console.log(token);
-    //
-    // res.end(token);
-
-
-    fetch('https://github.com/login/oauth/access_token', {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
         method: 'post',
         body: {
             client_id: '2e80edb9aac1beb5e0cf',
@@ -36,15 +18,33 @@ app.post('/token/:code', async function (req, res) {
         },
         headers: {
             'Content-Type': 'application/json'
-        }
-    }).then(function (response) {
-        const data = JSON.stringify(response.body);
-        console.log(data);
-        res.send(data);
-        return response;
-    }).catch(function () {
-        console.log("Token nicht erhalten");
+        },
     });
+
+    const token = await JSON.stringify(response.body);
+    console.log(token);
+
+    res.send(token);
+
+
+    // fetch('https://github.com/login/oauth/access_token', {
+    //     method: 'post',
+    //     body: {
+    //         client_id: '2e80edb9aac1beb5e0cf',
+    //         client_secret: '19409ed971477475776611a5079a34d2ca0cc360',
+    //         code: code
+    //     },
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(function (response) {
+    //     const data = JSON.stringify(response.body);
+    //     console.log(data);
+    //     res.send(data);
+    //     return response;
+    // }).catch(function () {
+    //     console.log("Token nicht erhalten");
+    // });
 });
 
 var server = app.listen(8081, function () {
